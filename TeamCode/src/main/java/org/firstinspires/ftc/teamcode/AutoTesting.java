8bafab4adb77d67ac17cfe4093a17a2f5a027f36
suman <sumantmunagala@gmail.com>
2022-01-30 11:09:15 -0800

    autotest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTesting.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+@Autonomous(name = "AutoTest")
+public class AutoTesting extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        Pose2d startPose = new Pose2d(0,0,0);
+
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        drive.setPoseEstimate(startPose);
+
+        Trajectory moveForward = drive.trajectoryBuilder(startPose)
+                .forward(40)
+                .build();
+
+        Trajectory strafeToStart = drive.trajectoryBuilder(moveForward.end())
+                .strafeTo(new Vector2d(0,0))
+                .build();
+
+        drive.followTrajectory(moveForward);
+        drive.followTrajectory(strafeToStart);
+    }
+}
