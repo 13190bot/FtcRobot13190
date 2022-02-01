6b9d22f150cc16aea80c41eb216fc2e024916925
mhuynh5 <melvinhuynh5@gmail,com>
2022-01-31 17:06:42 -0800

    added teleOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freighfrenzy/FinalTeleOp.java
@@ -0,0 +1,102 @@
+package org.firstinspires.ftc.teamcode.freighfrenzy;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+@TeleOp
+public class FinalTeleOp extends template {
+
+    private DcMotor frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor, duckMotor;
+    public TouchSensor limit;
+    static final double MAX_POSITION = 1;
+    static final double MIN_POSITION = 0.3;
+
+    public void runOpMode() {
+        frontLeftMotor = hardwareMap.dcMotor.get("leftFront");
+        rearLeftMotor = hardwareMap.dcMotor.get("leftRear");
+        frontRightMotor = hardwareMap.dcMotor.get("rightFront");
+        rearRightMotor = hardwareMap.dcMotor.get("rightRear");
+        duckMotor = hardwareMap.dcMotor.get("duckMotor");
+        limit = hardwareMap.touchSensor.get("limit");
+
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        initialize();
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double horizontal = -gamepad1.left_stick_x;
+            double vertical = -gamepad1.left_stick_y;
+            double angle = -gamepad1.right_stick_x;
+
+            drive.setWeightedDrivePower(new Pose2d(vertical, horizontal, angle)); // in roadrunner x is vertical and y is horizontal
+            drive.update();
+
+            if (this.gamepad1.left_bumper) {
+                duckMotor.setPower(-0.5);
+            } else {
+                duckMotor.setPower(0);
+            }
+
+            if (this.gamepad1.right_trigger > 0.4) {
+                duckMotor.setPower(0.5);
+            } else {
+                duckMotor.setPower(0);
+            }
+
+            telemetry.addData("FrontLeftPower", frontLeftMotor.getPower());
+            telemetry.addData("BackLeftPower", rearLeftMotor.getPower());
+            telemetry.addData("FrontRightPower", frontRightMotor.getPower());
+            telemetry.addData("BackRightPower", rearRightMotor.getPower());
+
+            telemetry.addData("rotationPosition", armRotationMotor.getCurrentPosition());
+            telemetry.addData("intakeMotorPower", intakeMotor.getPower());
+            telemetry.addData("limitSwitch", limit.isPressed());
+            telemetry.addData("servoPos", directionServo.getPosition());
+            telemetry.addData("armPower", armRotationMotor.getPower());
+            if (limit.isPressed()) {
+                armRotationMotor.setPower(0);
+                armRotationMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                armRotationMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                telemetry.addData("ERROR: ", "too far");
+            }
+            if (gamepad2.right_trigger > 0.2) {
+                intakeMotor.setPower(1);
+            } else if (gamepad2.left_trigger > 0.2) {
+                intakeMotor.setPower(-1);
+            } else {
+                intakeMotor.setPower(0);
+            }
+            double x = gamepad2.left_stick_x;
+            telemetry.addData("arm almoa", x);
+            if (armRotationMotor.getCurrentPosition() < 800 && x > 0) {
+                armRotationMotor.setPower(x * 0.35);
+            } else if (armRotationMotor.getCurrentPosition() > 800 && x > 0) {
+                armRotationMotor.setPower(x * 0.5);
+            } else if (armRotationMotor.getCurrentPosition() > 800 && x < 0) {
+                armRotationMotor.setPower(x * 0.35);
+            } else {
+                armRotationMotor.setPower(x * 0.5);
+            }
+
+            if (gamepad2.left_bumper) {
+                armRotationMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                armRotationMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            }
+            double servoInput = gamepad2.right_stick_x;
+            if (servoInput > 0.2 && directionServo.getPosition() < MAX_POSITION) {
+                directionServo.setPosition(directionServo.getPosition() + 0.02);
+            }
+            if (servoInput < -0.2 && directionServo.getPosition() > MIN_POSITION) {
+                directionServo.setPosition(directionServo.getPosition() - 0.02);
+            }
+            telemetry.update();
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freighfrenzy/template.java
@@ -0,0 +1,73 @@
+package org.firstinspires.ftc.teamcode.freighfrenzy;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public abstract class template extends LinearOpMode {
+
+    static final double COUNTS_PER_MOTOR_REV = 537.7;
+    static final double GEAR_CHANGE = 4.0;
+
+    public DcMotor armRotationMotor;
+    public DcMotor intakeMotor;
+    public Servo directionServo;
+    public boolean manual = false;
+
+    public void initialize(){
+        telemetry.addData("Status", "Initializing Arm Motors");
+        telemetry.update();
+        armRotationMotor = hardwareMap.get(DcMotor.class, "rotationMotor");
+        armRotationMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
+        intakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        directionServo = hardwareMap.get(Servo.class, "directionServo");
+    }
+    public boolean isRotationBusy(){
+        if(armRotationMotor.isBusy()){
+            return true;
+        }else{
+            if(armRotationMotor.getMode() != DcMotor.RunMode.RUN_USING_ENCODER ) {
+                armRotationMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            }if(armRotationMotor.getPower() != 0 && manual == false){
+                armRotationMotor.setPower(0);
+            }
+            return false;
+        }
+    }
+    public void toPickupPosition(){
+        armRotationMotor.setTargetPosition(0);
+        armRotationMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armRotationMotor.setPower(0.5);
+        directionServo.setPosition(0.66);
+    }
+    public void toBottomLevel(){
+        double targetAngle = 155;
+        moveThings(targetAngle);
+        directionServo.setPosition(0.3);
+    }
+    public void toMiddleLevel(){
+        double targetAngle = 130;
+        moveThings(targetAngle);
+        directionServo.setPosition(0.33);
+    }
+    public void toTopLevel(){
+        double targetAngle = 95;
+        moveThings(targetAngle);
+        directionServo.setPosition(0.51);
+    }
+    /*
+    public boolean isRotationTooFar(){
+        if(armRotationMotor.getCurrentPosition() > 180*COUNTS_PER_MOTOR_REV*GEAR_CHANGE/360 && armRotationMotor.getPower()>0){
+            return true;
+        }else return false;
+    }*/
+    private void moveThings(double targetAngle){
+        int targetPosition = (int) (targetAngle*COUNTS_PER_MOTOR_REV*GEAR_CHANGE)/360;
+        armRotationMotor.setTargetPosition(targetPosition);
+        armRotationMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armRotationMotor.setPower(0.5);
+    }
+}
