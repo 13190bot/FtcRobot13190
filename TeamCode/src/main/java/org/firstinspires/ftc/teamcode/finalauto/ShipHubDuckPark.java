cea6d3a9208c184a97f3e71167b9b5b016c3325c
suman <sumantmunagala@gmail.com>
2022-02-03 15:14:09 -0800

    added shippinghub duck park

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/finalauto/ShipHubDuckPark.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode.finalauto;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+@Autonomous(name = "ShipHubDuckPark")
+public class ShipHubDuckPark extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        Pose2d startPose = new Pose2d(0, 0, 0);
+
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        drive.setPoseEstimate(startPose);
+
+        Trajectory shippingHub = drive.trajectoryBuilder(startPose)
+                .strafeTo(new Vector2d(20, 40))
+                .build();
+
+        Trajectory duck = drive.trajectoryBuilder(shippingHub.end())
+                .lineToLinearHeading(new Pose2d(2, 27, Math.toRadians(-107)))
+                .build();
+
+        Trajectory warehouse = drive.trajectoryBuilder(duck.end())
+                .forward(85)
+                .build();
+
+        Trajectory warehouse_2 = drive.trajectoryBuilder(warehouse.end())
+                .strafeLeft(35)
+                .build();
+
+        Trajectory warehouse_3 = drive.trajectoryBuilder(warehouse_2.end())
+                .forward(42)
+                .build();
+
+
+
+        drive.followTrajectory(shippingHub);
+        //Do arm stuff
+        drive.followTrajectory(duck);
+        drive.followTrajectory(warehouse);
+        drive.followTrajectory(warehouse_2);
+        drive.followTrajectory(warehouse_3);
+    }
+}
