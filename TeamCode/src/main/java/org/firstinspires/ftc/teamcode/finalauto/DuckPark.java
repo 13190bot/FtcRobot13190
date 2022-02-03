94fb71d49bf8ead69cbb0fb42332d72345825fe4
wilcoxrobotics <teamrobotics8872@gmail.com>
2022-02-02 17:19:23 -0800

    auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/finalauto/DuckPark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/finalauto/DuckPark.java
@@ -1,27 +1,56 @@
 package org.firstinspires.ftc.teamcode.finalauto;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
-@Autonomous(name = "DuckTest")
+@Autonomous(name = "DuckPark")
 public class DuckPark extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
+        DcMotor duckMotor;
 
         Pose2d startPose = new Pose2d(0, 0, Math.toRadians(0));
 
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        duckMotor = hardwareMap.get(DcMotor.class, "duckMotor");
+
 
         drive.setPoseEstimate(startPose);
 
-        Trajectory duck = drive.trajectoryBuilder(startPose)
-                .lineToLinearHeading(new Pose2d(0, 30, Math.toRadians(135)))
+        Trajectory forward = drive.trajectoryBuilder(startPose)
+                .forward(40)
+                .build();
+        Trajectory duck = drive.trajectoryBuilder(forward.end())
+                .lineToLinearHeading(new Pose2d(2, 27, Math.toRadians(-107)))
                 .build();
 
+        Trajectory warehouse = drive.trajectoryBuilder(duck.end())
+                .forward(85)
+                .build();
+
+        Trajectory warehouse_2 = drive.trajectoryBuilder(warehouse.end())
+                .strafeLeft(35)
+                .build();
+
+        Trajectory warehouse_3 = drive.trajectoryBuilder(warehouse_2.end())
+                .forward(40)
+                .build();
+
+
+
+        drive.followTrajectory(forward);
         drive.followTrajectory(duck);
+        duckMotor.setPower(0.5);
+        sleep(4000);
+        duckMotor.setPower(0);
+        drive.followTrajectory(warehouse);
+        drive.followTrajectory(warehouse_2);
+        drive.followTrajectory(warehouse_3);
     }
 }
