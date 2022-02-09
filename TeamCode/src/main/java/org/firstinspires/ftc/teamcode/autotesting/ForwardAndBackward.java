b4ac68cb66d63e873e8aae635fc6d8260f3d6bb2
suman <sumantmunagala@gmail.com>
2022-02-08 17:06:25 -0800

    forward and backward cuz ayan seemingly wanted it

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autotesting/ForwardAndBackward.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.autotesting;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+@Autonomous(name = "ForwardAndBackward")
+public class ForwardAndBackward extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        Pose2d startPose = new Pose2d(0, 0, 0);
+
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        drive.setPoseEstimate(startPose);
+
+        Trajectory forward = drive.trajectoryBuilder(startPose)
+                .forward(50)
+                .build();
+
+        Trajectory backward = drive.trajectoryBuilder(forward.end())
+                .back(50)
+                .build();
+
+        while (true) {
+            drive.followTrajectory(forward);
+            drive.followTrajectory(backward);
+        }
+    }
+
+}
